76a85c3c041aaf04cee7bb39adc4da86687bba67
Rahul Paul <rp895993@gmail.com>
2025-11-08 11:47:53 +0530

    admin fix

--- track_changes.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Simple snapshot script for git changes
-cd /d %~dp0
-echo ------------------------------------------------------------ >> git-change-report.txt
-echo Snapshot taken at %DATE% %TIME% >> git-change-report.txt
-git status --porcelain >> git-change-report.txt
-echo   >> git-change-report.txt
-echo Snapshot appended to git-change-report.txt
-pause
